277b8913938b4f1b11ab73725deb32d277f8490a
Keep Calm <maslevian@yandex.ru>
2019-09-08 02:14:51 +0300

    Main loop with FPS

--- pygame_test.py
+++ pygame_test.py
@@ -2,6 +2,19 @@ import pygame
 
 pygame.init()
 
-pygame.display.set_mode((600, 400))
+s = pygame.display.set_mode((600, 400))
+# print(s)
+clock = pygame.time.Clock()
+FPS = 60
 
+t = 0
+while 1:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    # print(t)
+    # t += 1
+    clock.tick(FPS)
 
