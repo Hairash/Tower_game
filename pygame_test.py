c06969132e5972c6c0b63c74ef274b7669043ac5
Keep Calm <maslevian@yandex.ru>
2019-09-08 12:49:34 +0300

    First blood

--- pygame_test.py
+++ pygame_test.py
@@ -2,19 +2,30 @@ import pygame
 
 pygame.init()
 
-s = pygame.display.set_mode((600, 400))
-# print(s)
-clock = pygame.time.Clock()
+# consts
 FPS = 60
 
-t = 0
+COLORS = {
+    'white': (255, 255, 255),
+    'red': (255, 0, 0)
+}
+
+# create drawing field
+field_surface = pygame.display.set_mode((600, 400))
+
+clock = pygame.time.Clock()
+
+pygame.draw.rect(field_surface, COLORS['red'], (20, 20, 100, 80))
+
+pygame.display.update()
+
+# main loop
 while 1:
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # print(t)
-    # t += 1
+
     clock.tick(FPS)
 
