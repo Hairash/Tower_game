7f54119d9e6102d38fea5f50db8c52ea7b7497f9
Hairash <maslevian@yandex.ru>
2024-06-30 01:50:30 +0200

    Perfect camera

--- src/camera.py
+++ src/camera.py
@@ -2,28 +2,58 @@ import pygame
 
 
 class Camera:
-    def __init__(self):
+    MOUSE_SPEED = 0.5
+    ZOOM_MIN = 0.5
+    ZOOM_MAX = 2
+    ZOOM_STEP = 0.03
+    BORDER_PERCENT = 0.15
+
+    def __init__(self, field_width, field_height):
         self.display_surface = pygame.display.get_surface()
-        self.half_width = self.display_surface.get_size()[0] // 2
-        self.half_height = self.display_surface.get_size()[1] // 2
+        self.screen_width = self.display_surface.get_size()[0]
+        self.screen_height = self.display_surface.get_size()[1]
+
+        self.field_width = field_width
+        self.field_height = field_height
 
         # mouse
-        self.x_border_offset = self.half_width * 0.3
-        self.y_border_offset = self.half_height * 0.3
-        self.mouse_speed = 0.2
+        self.x_border_offset = self.screen_width * self.BORDER_PERCENT
+        self.y_border_offset = self.screen_height * self.BORDER_PERCENT
 
         self.offset = pygame.math.Vector2()
 
         # zoom
         self.zoom_scale = 1
-        # TODO: make consts
-        self.internal_surface_size = (2600, 2600)
-        self.internal_surface = pygame.Surface(self.internal_surface_size, pygame.SRCALPHA)
-        self.internal_rect = self.internal_surface.get_rect(center=(self.half_width, self.half_height))
-        self.internal_surface_size_vector = pygame.math.Vector2(self.internal_surface_size)
-        self.internal_offset = pygame.math.Vector2()
-        self.internal_offset.x = self.internal_surface_size[0] // 2 - self.half_width
-        self.internal_offset.y = self.internal_surface_size[1] // 2 - self.half_height
+
+    @property
+    def max_offset_x(self):
+        return self.field_width - self.screen_width / self.zoom_scale
+
+    @property
+    def max_offset_y(self):
+        return self.field_height - self.screen_height / self.zoom_scale
+
+    # @property
+    # def max_width(self):
+    #     return self.field_width * self.zoom_scale
+    #
+    # @property
+    # def max_height(self):
+    #     return self.field_height * self.zoom_scale
+
+    @property
+    def visible_field_width(self):
+        if self.max_offset_x > 0:
+            return self.screen_width
+        else:
+            return self.field_width * self.zoom_scale
+
+    @property
+    def visible_field_height(self):
+        if self.max_offset_y > 0:
+            return self.screen_height
+        else:
+            return self.field_height * self.zoom_scale
 
     def mouse_control(self):
         mouse = pygame.math.Vector2(pygame.mouse.get_pos())
@@ -56,20 +86,36 @@ class Camera:
             if mouse.y > bottom_border:
                 mouse_offset_vector.y = bottom_border - mouse.y
 
-        self.offset += mouse_offset_vector * self.mouse_speed
-
-    def draw(self, surface):
-        scaled_surface = pygame.transform.scale(
-            self.internal_surface,
-            self.internal_surface_size_vector * self.zoom_scale,
-        )
-        scaled_rect = scaled_surface.get_rect(center=(self.half_width, self.half_height))
-
-        surface.blit(scaled_surface, scaled_rect)
+        mouse_offset_vector = -mouse_offset_vector
+        if self.max_offset_x > 0:
+            self.offset.x += mouse_offset_vector.x * self.MOUSE_SPEED
+            if self.offset.x < 0:
+                self.offset.x = 0
+            elif self.offset.x > self.max_offset_x:
+                self.offset.x = self.max_offset_x
+        else:
+            self.offset.x = self.max_offset_x / 2
+        if self.max_offset_y > 0:
+            self.offset.y += mouse_offset_vector.y * self.MOUSE_SPEED
+            if self.offset.y < 0:
+                self.offset.y = 0
+            elif self.offset.y > self.max_offset_y:
+                self.offset.y = self.max_offset_y
+        else:
+            self.offset.y = self.max_offset_y / 2
 
     def get_mouse_coords_on_field(self, mouse_pos):
-        scaled_coords = pygame.math.Vector2(
-            self.half_width + (mouse_pos[0] - self.half_width) / self.zoom_scale,
-            self.half_height + (mouse_pos[1] - self.half_height) / self.zoom_scale,
-        )
-        return scaled_coords - self.offset
+        return pygame.math.Vector2(mouse_pos) / self.zoom_scale + self.offset
+
+    def make_zoom(self, zoom_change):
+        center_x = self.offset.x + self.screen_width / 2 / self.zoom_scale
+        center_y = self.offset.y + self.screen_height / 2 / self.zoom_scale
+
+        self.zoom_scale += zoom_change * self.ZOOM_STEP
+        if self.zoom_scale > self.ZOOM_MAX:
+            self.zoom_scale = self.ZOOM_MAX
+        if self.zoom_scale < self.ZOOM_MIN:
+            self.zoom_scale = self.ZOOM_MIN
+
+        self.offset.x = center_x - self.screen_width / 2 / self.zoom_scale
+        self.offset.y = center_y - self.screen_height / 2 / self.zoom_scale
